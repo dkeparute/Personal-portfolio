2f17548146a9b5e8b93ad0475db23f07d789ae25
Dovilė <dovile.keparute@gmail.com>
2021-12-15 18:35:17 +0200

    Blog

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ const skills_bars = document.querySelectorAll(".skill-progress");
 
 window.addEventListener("scroll", () => {
   skillsEffect();
+  countUp();
 });
 
 function checkScroll(el) {
@@ -37,3 +38,29 @@ function skillsEffect() {
   skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
 }
 // -------------------------------------------------------------
+
+const records_wrap = document.querySelector(".records");
+const records_numbers = document.querySelectorAll(".number");
+
+function countUp() {
+  if (!checkScroll(records_wrap)) return;
+  records_numbers.forEach((numb) => {
+    const updateCount = () => {
+      let currentNum = +numb.innerText;
+      let maxNum = +numb.dataset.num;
+      let speed = 100;
+      const increment = Math.ceil(maxNum / speed);
+
+      if (currentNum < maxNum) {
+        numb.innerText = currentNum + increment;
+        setTimeout(updateCount, 1);
+      } else {
+        numb.innerText = maxNum;
+      }
+    };
+
+    setTimeout(updateCount, 400);
+  });
+}
+// ---------------------------------------------------------------
+
