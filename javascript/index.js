f338de292d60e95f97b516051c1d3a55507ff104
Dovilė <dovile.keparute@gmail.com>
2021-12-23 15:48:45 +0200

    responsive

--- javascript/index.js
+++ javascript/index.js
@@ -25,3 +25,24 @@ function skillsEffect() {
     }
     skillsProgress.forEach((skill) => (skill.style.width = skill.dataset.progress));
 }
+
+// Hamburger menu
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
+
+function closeMenu() {
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+  }
+  
+  hamburger_menu.addEventListener("click", () => {
+    if (!navbar.classList.contains("open")) {
+      navbar.classList.add("open");
+      document.body.classList.add("stop-scrolling");
+    } else {
+      closeMenu();
+    }
+  });
+  
+  links.forEach((link) => link.addEventListener("click", () => closeMenu()));
